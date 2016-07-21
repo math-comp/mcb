5cb5fb3c2458295fe4dd57da976e0ddf460f485c
Enrico Tassi <gares@fettunta.org>
2016-07-21 17:30:19 +0200

    some smoothing and exercises for ch2

--- coq/ch2.v
+++ coq/ch2.v
@@ -36,3 +36,27 @@ rewrite dvdn_addr.
   by rewrite gtnNdvd // prime_gt1.
 by rewrite dvdn_fact // prime_gt0.
 Qed.
+
+Lemma orbA b1 b2 b3 : b1 || (b2 || b3) = b1 || b2 || b3.
+Proof. by case: b1; case: b2; case: b3. Qed.
+Lemma implybE a b : (a ==> b) = ~~ a || b.
+Proof. by case: a; case: b. Qed.
+Lemma negb_and (a b : bool) : ~~ (a && b) = ~~ a || ~~ b.
+Proof. by case: a; case: b. Qed.
+
+Definition all_words n T (alphabet : seq T) :=
+  let prepend x wl := [seq x :: w | w <- wl] in
+  let extend wl := flatten [seq prepend x wl | x <- alphabet] in
+  iter n extend [::[::]].
+Arguments all_words _ [T] _.
+
+
+Lemma size_all_words n T (alphabet : seq T) :
+  size (all_words n alphabet) = size alphabet ^ n.
+Proof.
+elim: n => [|n IHn]; first by rewrite expn0.
+rewrite expnS -{}IHn [in LHS]/all_words iterS -/(all_words _ _).
+elim: alphabet (all_words _ _) => //= w ws IHws aw.
+by rewrite size_cat IHws size_map mulSn.
+Qed.
+
