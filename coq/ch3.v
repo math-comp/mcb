8736c69854f4153ed2ecf385cb12dc95d5ac8e9f
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-09-07 18:46:01 +0200

    new script for strong induction, todo comment

--- coq/ch3.v
+++ coq/ch3.v
@@ -18,14 +18,13 @@ Check Prop.
 
 Lemma stamps n : 12 <= n -> exists s4 s5, s4 * 4 + s5 * 5 = n.
 Proof.
-elim: n {-2}n (leqnn n) =>[|n IHn]; first by case.
-do 12! [ case=> // ].
-case; first by exists 3, 0.
-case; first by exists 2, 1.
-case; first by exists 1, 2.
-case; first by exists 0, 3.
-move=> m'; set m := _.+1; move=> mn m11.
-case: (IHn (m-4) _ isT) => [|s4 [s5 def_m4]].
-  by rewrite leq_subLR (leq_trans mn) // addSnnS leq_addl.
+have [m leq_mn] := ubnPgeq n; elim: n => // n IHn in m leq_mn *; first by case: n => [|n] in IHn *.
+do 12! [ case: m => //= m in leq_mn * ] => _.
+case: m => [|m] in leq_mn *; first by exists 3, 0.
+case: m => [|m] in leq_mn *; first by exists 2, 1.
+case: m => [|m] in leq_mn *; first by exists 1, 2.
+case: m => [|m] in leq_mn *; first by exists 0, 3.
+case: (IHn ((16+m) - 4) _ isT) => [|s4 [s5 def_m4]]. 
+  by rewrite leq_subLR (leq_trans leq_mn) // addSnnS leq_addl.
 by exists s4.+1, s5; rewrite mulSn -addnA def_m4 subnKC.
 Qed.
